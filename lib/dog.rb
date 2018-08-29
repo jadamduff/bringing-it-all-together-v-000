6ee9c6d22c0ee2b25205bba86eb3ea7096c2d931
Adam Duff <j.adam.duff@gmail.com>
2018-08-29 15:52:04 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+  attr_accessor :name, :breed
+  attr_reader :id
+
+  def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+        CREATE TABLE dogs (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          breed TEXT
+        );
+      SQL
+  end
+
+end
